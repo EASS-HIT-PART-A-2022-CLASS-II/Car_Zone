7cf1533c1446eec4ccdc0ee500cda8a89ca1ce19
reut ohayon <66826644+reut201112@users.noreply.github.com>
2022-12-18 18:40:31 +0200

    get color function

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ def tst(car_id:int):
             return i
     return json_obj 
 
-@app.post("/tstpost",response_model=carRes)
+@app.post("/tstpost",response_model=carRes,response_model_exclude_unset=True)
 def getprice(car:carPost):
     res=requests.get("http://data.gov.il/api/3/action/datastore_search?resource_id=053cea08-09bc-40ec-8f7a-156f0677aff3")
     json_obj=res.json()
@@ -31,22 +31,26 @@ def getprice(car:carPost):
         if i['tozeret_cd']==car.manifacture & i['degem_cd']==car.modle:
             carRes.id=i['_id']
             carRes.mispar_rechev=i['mispar_rechev']
-            return {carRes}
-    return json_obj 
+            return carRes
+    carRes.error_code="500"
+    carRes.id=0000
+    return carRes
 
-@app.get("/getcar/{car_num}",response_model=carRes)
-def getcar(car_num:int,car:carRes):
+
+@app.get("/getcarcolor/{car_num}",response_model=carRes,response_model_exclude_unset=True)
+def getcarcolor(car_num:int):
     res=requests.get("http://data.gov.il/api/3/action/datastore_search?resource_id=053cea08-09bc-40ec-8f7a-156f0677aff3")
     json_obj=res.json()
     for i in json_obj['result']['records']:
         if i['mispar_rechev']==car_num:
-           # car = carRes(
-            #    id=i['_id'],
-            #    mispar_rechev=i['mispar_rechev'],
-            #    tozeret_cd=i['tozeret_cd']
-            #)
-            return {i}
-    return json_obj 
+           carRes.id=i['_id']
+           carRes.tzeva_rechev=i['tzeva_rechev']
+           carRes.error_code="ok"
+           return carRes
+    carRes.id=0000
+    carRes.error_code="ok"
+    return carRes
+   
 
 
 if __name__ == "__main__":
--- src/models.py
+++ src/models.py
@@ -2,20 +2,24 @@ from pydantic import BaseModel
 from typing import Optional
 
 class carPost(BaseModel):
-    manifacture:int
-    modle:int
+    manifacture: Optional[int]
+    modle: Optional[int]
 
 class carRes(BaseModel):
     id : int
     mispar_rechev : Optional[int]
     sug_degem : Optional[str]
-    degem_cd : Optional[int]
+    degem_cd : Optional[str]
     degem_nm : Optional[str]
-    tozeret_cd : Optional[int]
+    tozeret_cd : Optional[str]
     tozeret_nm : Optional[str]
-    tzeva_cd : Optional[int]
+    tzeva_cd : Optional[str]
     tzeva_rechev : Optional[str]
     moed_aliya_lakvish : Optional[str]
+    error_code : Optional[str]
+    class Config:
+        orm_mode = True
+
     
 
 
