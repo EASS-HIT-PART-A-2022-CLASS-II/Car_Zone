3bfcaa99e8cfbea3ae5614ffc267d72a56d2d859
reut ohayon <66826644+reut201112@users.noreply.github.com>
2022-12-27 13:48:46 +0200

    fix backend

--- FrontEnd/main.py
+++ FrontEnd/main.py
@@ -13,16 +13,16 @@ def get_cars_by_manufacturer(manifacture: str, modle: str):
     response = requests.post("http://localhost:8000/getcarsbymanyfacture", json=data)
     return response.json()
 
-def get_car_color(car_num):
-  response = requests.get("http://localhost:8000/getcarcolor/{car_num}")
+def get_car_color(car_num:str):
+  response = requests.get(f"http://localhost:8000/getcarcolor/{car_num}")
   return response.json()
 
 def main():
   sts.title("Car Zone")
 
   all_cars_button = sts.button("Get All Cars")
-  manufacturer_button = sts.button("Get Cars by Manufacturer")
-  #color_button = sts.button("Get Car Color")
+  manufacturer_button = sts.button("Get Cars by Manufacturer and modle")
+  color_button = sts.button("Get Car Color by car number")
 
   if all_cars_button:
       all = get_all_cars()
@@ -41,45 +41,44 @@ def main():
         args=([st.session_state.manifacture + " " + st.session_state.modle]),
     ) 
 
-  elif st.sidebar.button("Get data from backend"):
+  elif color_button:
       if "car" not in st.session_state:
         st.session_state.car = "4284078"
       st.button(
-        "Get color",
+        "Get car color",
         on_click=get_color(st.session_state.car),
       )
-    
+
 def get_car_by(car):
-  with st.form(key="test", clear_on_submit=True):
+  with st.form(key="manifacture", clear_on_submit=True):
         col1, col2 = st.columns(2)
         manifacture = col1.text_input("Manifacture", car.split()[0], key="manifacture")
         modle = col2.text_input("Modle", car.split()[-1], key="modle")
         submit = st.form_submit_button(
-            "Submit", on_click=show_car
+            "Get car", on_click=show_car
         )
 
 def show_car():
     car = get_cars_by_manufacturer(st.session_state.manifacture,st.session_state.modle)
-    if car:
+    if car['car'][0]['error_code']:
+      st.write(f"{car['car'][0]['mispar_rechev']}")
       st.json(car)
 
 
 def get_color(car_num):
-      with st.form(key="test", clear_on_submit=True):
-        col1,col2 = st.columns(2)
-        carcolor = col1.text_input("Enter car number", car_num, key="car")
+      with st.form(key="car", clear_on_submit=True):
+        col1 = st.columns(1)
+        carcolor = col1[0].text_input("Enter car number", car_num, key="car")
         submit = st.form_submit_button(
-            "Submit", on_click=show_color
+            "Get color", on_click=show_color
         )
         
-
 def show_color():
-  st.write(st.session_state.car)
-  entercar=int(st.session_state.car)+0
-  st.write(type(entercar))
-  res=get_car_color(entercar)
-  st.json(res)
-
+  res=get_car_color(st.session_state.car)
+  if res['error_code']==200:
+    st.write(f"The color of the car is : {res['tzeva_rechev']}")
+  else:
+    st.write("car not found")
 
 if __name__ == "__main__":
     main()
